2a6b490234d9b0beac120e22b00e49b8279fea24
edouard <edouard.blais@hotmail.com>
2022-12-04 21:33:23 -0500

    fixed issue with link to new route

--- routes/new.js
+++ routes/new.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET new page. */
-router.get('/new', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('form')
-  })
+})
 
 module.exports = router;
